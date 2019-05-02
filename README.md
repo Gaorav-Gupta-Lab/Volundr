78b0ae7b7e4ac2999b64dcc138672dee0a0ba1a7
Dennis Simpson <g2.checkpoint@gmail.com>
2019-05-02 16:33:50 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Volundr
+### CRISPR Based Synthetic Lethal Screens in Mammalian Cells
+## Cite
+
+[![PubMed](img/2318832.png)](https://www.ncbi.nlm.nih.gov/pubmed/28649649)
---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_namespace_packages
+import sys
+
+if sys.version_info < (3, 5):
+    sys.stdout.write("At least Python 3.5 is required.\n")
+    sys.exit(1)
+# packages=['volundr', 'Valkyries'],
+
+setup(
+    name='Volundr',
+    version='1.0.0',
+    packages=find_namespace_packages(include=['Volundr.*']),
+
+    url='',
+    license='MIT',
+    author='Dennis Simpson',
+    author_email='dennis@email.unc.edu',
+    long_description=open('README.md').read(),
+
+    description='Package for using CRISPR to do synthetic lethal and viable assays in mammalian cells',
+    install_requires=['scipy', 'natsort', 'pysam', 'python-magic', 'pathos', 'numpy']
+)
